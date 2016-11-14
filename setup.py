38d035652ffe9edd3dedf183fd6e11a44f8b6df0
Mihai Dinca <mdinca@suse.de>
2016-11-14 16:09:52 +0100

    Increase dev version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-salt-containers',
-    version='0.2.dev1',
+    version='0.2.dev2',
     author='Mihai Dinca',
     author_email='dincamihai@gmail.com',
     maintainer='Mihai Dinca',
